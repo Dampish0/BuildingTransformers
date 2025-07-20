Forgot to add masking and attempted to fix issue
model has extremely low loss during training which is probably a problem with masking.

model.py
@@ -6,22 +6,33 @@ torch.manual_seed(42)
 
 
 class Head(nn.Module):
-    def __init__(self, n_embd, n_head):
+    def __init__(self, n_embd, n_head, blocksize, masked=False):
         super().__init__()
         self.d_head = n_embd // n_head
+        self.n_embd = n_embd
+        self.masked = masked
 
-        self.weightQ = nn.Linear(n_embd,n_embd)
-        self.weightK = nn.Linear(n_embd,n_embd)
-        self.weightV = nn.Linear(n_embd,n_embd)
-        
+        self.weightQ = nn.Linear(n_embd,self.d_head)
+        self.weightK = nn.Linear(n_embd,self.d_head)
+        self.weightV = nn.Linear(n_embd,self.d_head)
+        if masked:
+            self.register_buffer("mask", (torch.tril(torch.ones([blocksize, blocksize]), 1).unsqueeze(0) == 0))
 
     def forward(self, x):
-        k = self.weightK(x)
-        q = self.weightQ(x)
-        v = self.weightV(x)
-        
-        h = q @ (torch.transpose(k, 1,2))
+        b,t,c = x.size()
+        # x = b, t , n_embd
+        k = self.weightK(x) # n_embd, d_head
+        q = self.weightQ(x) # n_embd, d_head
+        v = self.weightV(x) # n_embd, d_head
+        # print(q.shape)
+        # print((torch.transpose(k, 1,2)).shape)
+        h = q @ (torch.transpose(k, 1,2)) # b, t, d_head  x  b, d_head, t  =  b, t, t
+        #print(h.shape)
         h = h / math.sqrt(self.d_head)
+
+        if(self.masked):
+            #forgot masking
+            h = h.masked_fill(self.mask[:, :t, :t], float('-inf'))
 
         y = torch.softmax(h, dim=-1)
 
@@ -35,17 +46,17 @@ class Head(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_embd, n_head):
+    def __init__(self, n_embd, n_head, blocksize, masked=False):
         super().__init__()       
         self.n_head = n_head
-        self.heads = nn.ModuleList([Head(n_embd, n_head) for i in range(n_head)])
-        self.weight0 = nn.Linear(n_embd*n_head, n_embd)
+        self.heads = nn.ModuleList([Head(n_embd, n_head, blocksize, masked) for i in range(n_head)])
+        self.weight0 = nn.Linear(n_embd, n_embd)
 
     def forward(self, x):
-
-        z = self.heads[0](x)
-        for i in range(self.n_head-1):
-           z = torch.concat([z, self.heads[i+1](x)], dim=2)
+        # b, t, c
+        z = [self.heads[i](x) for i in range(len(self.heads))]
+        z = torch.concat(z, dim=2)
+        
 
         k = self.weight0(z)
         return k
@@ -53,18 +64,21 @@ class MultiHeadAttention(nn.Module):
 
 
 class Block(nn.Module):
-    def __init__(self, n_embd, n_head):
+    def __init__(self, n_embd, n_head, blocksize):
         super().__init__()
 
-        self.atten = MultiHeadAttention(n_embd, n_head)
+        self.atten = MultiHeadAttention(n_embd, n_head, blocksize, masked=True)
+        self.atten2 = MultiHeadAttention(n_embd, n_head, blocksize, masked=False)
         self.ffwd = mlp(n_embd)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
+        self.ln3 = nn.LayerNorm(n_embd)
 
 
     def forward(self, x):
-        x = x + self.ln1(self.atten(x))
-        x = x + self.ln2(self.ffwd(x))
+        x = x + self.atten(self.ln1(x))
+        x = x + self.atten2(self.ln2(x))
+        x = x + self.ffwd(self.ln3(x))
         return x
     
 
@@ -83,19 +97,22 @@ class mlp(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, n_layer, n_embd, n_head, vocab_size):
+    def __init__(self, n_layer, n_embd, n_head, vocab_size, blockSize):
         super().__init__()
-        self.Layers = nn.ModuleList([Block(n_embd, n_head) for i in range(n_layer)])
+        self.Layers = nn.ModuleList([Block(n_embd, n_head, blockSize) for i in range(n_layer)])
         self.tokEmb = nn.Embedding(vocab_size, n_embd)
+        self.posEmb = nn.Embedding(blockSize, n_embd)
         self.lmHead = nn.Linear(n_embd, vocab_size)
+        self.lnf = nn.LayerNorm(n_embd)
 
     def forward(self, x, target = None):
-        
+        b,t = x.size()
         x = self.tokEmb(x)
-
+        pos_emb = self.posEmb(torch.arange(t, device=x.device))
+        x = x + pos_emb
         for i in range(len(self.Layers)):
             x = self.Layers[i](x)
-        
+        x = self.lnf(x)
         x = self.lmHead(x)
 
         if(target != None):
@@ -105,9 +122,10 @@ class Model(nn.Module):
             return x, loss
         return x
 
-# testhead = Model(4,32,2,128).to("cuda")
-# x = torch.randint(low=0,high=32,size=[1,3000], dtype=torch.long, device="cuda")
-# y = torch.randint(low=0,high=32,size=[1,3000], dtype=torch.long, device="cuda")
-# out = testhead(x, y)
+if __name__ == "__main__":
+    testhead = Model(4,32,2,128, 3000).to("cuda")
+    x = torch.randint(low=0,high=32,size=[1,3000], dtype=torch.long, device="cuda")
+    y = torch.randint(low=0,high=32,size=[1,3000], dtype=torch.long, device="cuda")
+    out = testhead(x, y)
 
-# print(out)
+    print(out)

tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-0
+torch.tril(torch.ones([n_embd, n_embd]))

train.py
@@ -26,16 +26,16 @@ tokens = [vocab.index(v) for i, v in enumerate(data)]
 print(tokens[:40])
 print(vocab)
 
-maxSteps = 2
-gradAccum = 4
-batchSize = 4
-n_head = 8
-n_layers = 8
-n_embd = 256
+maxSteps = 1000
+gradAccum = 1
+batchSize = 16
+n_head = 12
+n_layers = 12
+n_embd = n_head * 32
 blocksize = 256
 
 
-model = Model(n_layers,n_embd,n_head,vocab_size).to("cuda")
+model = Model(n_layers,n_embd,n_head,vocab_size,blocksize).to("cuda")
 
 tot = 0
 for param in model.parameters():
@@ -44,24 +44,25 @@ for param in model.parameters():
 print(f"total params: {tot//1e6}M params")
 
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
 
 
 for i in range(maxSteps):
     optimizer.zero_grad()
-    for i in range(gradAccum):
+    for _ in range(gradAccum):
         x = torch.stack([torch.tensor(tokens[d * blocksize: (d+1) * blocksize], dtype=torch.long) for d in range(batchSize)]).to("cuda")
         y = torch.stack([torch.tensor(tokens[(d+1) * blocksize:(d+2) * blocksize], dtype=torch.long) for d in range(batchSize)]).to("cuda")
         out, loss = model(x, y)
         loss.backward()
     optimizer.step()
-    print(loss.item())
+    if i % 10 == 0:
+        print(f"step: {i}/{maxSteps}, loss: {loss.item()}")
 
 
 generated = torch.tensor([0], device="cuda").unsqueeze(0)
 from torch import nn
-for i in range(100):
+for i in range(200):
     out = model(generated)
     out = out[:,-1,:]
     