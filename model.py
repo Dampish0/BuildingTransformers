b2dc2eb5831400e6f62858910aac206174abbdb1
Dampish0 <elias.dovkrans@gmail.com>
2025-07-20 02:11:18 +0200

    Create model.py

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+
+
+class Head(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()       
+
+
+    def forward(x):
+
+
+        return
