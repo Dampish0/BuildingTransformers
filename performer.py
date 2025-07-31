f9c1a462827f1dce1acc79928c2b2f48c5c73f16
Dampish0 <elias.dovkrans@gmail.com>
2025-07-31 15:42:42 +0200

    Update performer.py

--- performer.py
+++ performer.py
@@ -1,8 +1,9 @@
 import torch
 import math
 import torch.distributions.multivariate_normal as dbs
+import torch.nn as nn
 
-M = torch.rand([32], dtype=torch.float32).unsqueeze(1)
+M = torch.rand([32], dtype=torch.float32).unsqueeze(0)
 dim = 32
 s = torch.zeros(dim)
 fn = dbs.MultivariateNormal(s, torch.eye(dim))#.sample()
@@ -10,29 +11,51 @@ omega = 8
 
 FNS = [torch.cos,torch.sin]
 
-def Aprox_softmax(x):
-    w = [fn.sample(x.size()) for _ in range(omega)]
-    print(w[0].shape, x.shape)
+def Aprox_softmax(k, q):
+    w = torch.concat([fn.sample(k.size()) for _ in range(omega)])
+    print(w.shape)
+    kNorm = torch.norm(k)
+    qNorm = torch.norm(q)
+    z = k + q
+    zSqr = torch.pow(kNorm, 2) +  torch.pow( qNorm, 2)
 
-    
+    A = torch.exp(-zSqr/2)
+    cosH = torch.cosh(w.transpose(1,2) @ z)
 
-    R = torch.concat([FNS[math.floor(int(i/omega))](torch.transpose(w[i%omega], 0, 2) @ x) for i in range(omega*len(FNS))], dim=0)
-    R = 1 * R
-    R = R / math.sqrt(omega)
-    print(R.shape)
+    R = (A @ w) * cosH
+
     return R
 
+class Head(nn.Module):
+    def __init__(self, n_embd, n_head, blocksize):
+        super().__init__()
+        self.d_head = n_embd // n_head
+        self.n_embd = n_embd
 
+        self.weightQ = nn.Linear(n_embd,self.d_head)
+        self.weightK = nn.Linear(n_embd,self.d_head)
+        self.weightV = nn.Linear(n_embd,self.d_head)
 
-msm = torch.softmax(M, dim=-1)
-masm = Aprox_softmax(M)
+    def forward(self, x):
+        
+        k = self.weightK(x) # n_embd, d_head
+        q = self.weightQ(x) # n_embd, d_head
+        v = self.weightV(x) # n_embd, d_head
 
-print(msm.shape, masm.shape)
+        Aprox_softmax(k,q)
 
-if(torch.allclose(msm, masm, rtol=1e-2, atol=1e-2)):
-    print("equal")
-else:
-    print("not equal")
+hd = Head(32,4,128,False)
+hd(M)
+
+# msm = torch.softmax(M, dim=-1)
+# masm = Aprox_softmax(M)
+
+# print(msm.shape, masm.shape)
+
+# if(torch.allclose(msm, masm, rtol=1e-2, atol=1e-2)):
+#     print("equal")
+# else:
+#     print("not equal")
 
 
 
